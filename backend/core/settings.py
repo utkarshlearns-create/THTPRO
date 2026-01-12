fbdd393bc2a5a297d8706fc01b20aec151957613
Utkarsh-loop <utkarsh94505@gmail.com>
2026-01-13 00:53:36 +0530

    Fix middleware configuration paths in settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,12 +48,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', # Added Whitenoise
-    'django.contrib.sessions.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.AuthenticationMiddleware',
-    'django.contrib.messages.MessageMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
