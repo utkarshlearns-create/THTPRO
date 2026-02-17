9e126ee9dee98f7d00454a1212a8070c1878030e
Utkarsh-loop <utkarsh94505@gmail.com>
2026-02-17 23:10:08 +0530

    fix(backend): Import viewsets in jobs/views.py

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions, status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
